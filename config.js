961539a09139c05997ef5ea272cb9b85a9e8df31
pankaj340 <pankajsahoo7853@gmail.com>
2024-12-22 03:03:10 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports={
+    MONGODB:'mongodb+srv://pankaj:hTaqeELtG3LssSnj@cluster0.f0kiy.mongodb.net/merng?retryWrites=true&w=majority&appName=Cluster0',
+    SECRET_KEY:'some very secret key'
+}
